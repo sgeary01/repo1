06b7a37bbe0c9fdc32bd20586c83fd78eb003da0
Sean Geary <sgeary@revenera.com>
2023-10-19 16:19:14 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sgeary01/repo1
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,22 @@
+/*
+Copyright 2023 Flexera Software LLC
+See LICENSE.TXT for full license text
+SPDX-License-Identifier: MIT
+
+Author : sgeary
+Created On : Thu Oct 19 2023
+File : main.go
+*/
+package main
+
+import (
+	"common_repo/folder1"
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("This is my main package for repo1")
+	folder1.Test1()
+
+}
